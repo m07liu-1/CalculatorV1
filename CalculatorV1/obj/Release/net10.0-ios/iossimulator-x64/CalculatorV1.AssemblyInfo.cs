10118f4780b131df9dd6adb4eee2508a306a0ce9
LiuMi <Michael.Liu@ec.gc.ca>
2026-09-23 11:19:44 -0400

    updated installer location

--- CalculatorV1/obj/Release/net10.0-ios/iossimulator-x64/CalculatorV1.AssemblyInfo.cs
+++ CalculatorV1/obj/Release/net10.0-ios/iossimulator-x64/CalculatorV1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CalculatorV1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+572a9a606a371feaf9933b7c00cca5d170b49f4f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+3fa8b04d0d25a858589b2c2bf8e6f4da4e3e92e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("CalculatorV1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CalculatorV1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
